578aad1a50f503acfe7bd26e529d596f5823ea25
Dmitry Shirokov <dshirokov@seek.com.au>
2019-03-06 21:50:31 +1100

    Fix primitive val types

--- test/index.js
+++ test/index.js
@@ -1,57 +1,65 @@
-'use strict';
+"use strict";
 
-const assert = require('assert');
-const sc = require('../lib');
-const schema = require('./data/schema');
+const assert = require("assert").strict;
+const sc = require("../lib");
+const schema = require("./data/schema");
 
-describe('schema-casting', function() {
-
-  it('should type cast string literals to appropriate types', () => {
+describe("schema-casting", function() {
+  it("should type cast string literals to appropriate types", () => {
     const output = sc(schema, {
-      'a': '1',
-      'b': 'true',
-      'c': '2.34',
-      'd': {
-        'e': {
-          'f': '5'
-        },
-      },
+      a: "1",
+			b: "true",
+			b0: "1",
+      c: "2.34",
+      d: {
+        e: {
+          f: "5"
+        }
+      }
     });
 
-    assert.deepStrictEqual(output, {
-      'a': 1,
-      'b': true,
-      'c': 2.34,
-      'd': {
-        'e': {
-          'f': 5
+    assert.deepEqual(output, {
+      a: 1,
+			b: true,
+			b0: true,
+      c: 2.34,
+      d: {
+        e: {
+          f: 5
         }
       }
     });
   });
 
-  it('should ignore string literals if casting unfeasible', () => {
+  it("should ignore string literals if casting unfeasible", () => {
     const output = sc(schema, {
-      'a': 'hello',
-      'b': true,
-      'c': 'float',
-      'd': {
-        'e': {
-          'f': '5'
-        },
-      },
+      a: "hello",
+      b: true,
+      c: "float",
+      d: {
+        e: {
+          f: "5"
+        }
+      }
     });
 
-    assert.deepStrictEqual(output, {
-      'a': 'hello',
-      'b': true,
-      'c': 'float',
-      'd': {
-        'e': {
-          'f': 5
+    assert.deepEqual(output, {
+      a: "hello",
+      b: true,
+      c: "float",
+      d: {
+        e: {
+          f: 5
         }
       }
     });
   });
 
+  it("should type cast constant string literals to appropriate types", () => {
+    const constantSchema = {
+      type: "integer"
+    };
+    const output = sc(constantSchema, "11");
+    assert.deepEqual(output, 11);
+  });
 });
